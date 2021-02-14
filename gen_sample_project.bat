3c64c414a9fb6c957e5f270083b5dcfaac6a9b88
Guilherme Oliveira <guilherme.061@gmail.com>
2021-02-13 21:14:49 -0500

    project is being generated correctly

---
+++ gen_sample_project.bat
@@ -0,0 +1,2 @@
+call realiti2D\thirdparty\bin\premake\premake5.exe vs2019
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,62 @@
+workspace "SampleProject"
+    architecture "x64"
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+    startproject "SampleProject"
+
+include "realiti2D/r2dstandalone.lua"
+
+project "SampleProject"
+    location "SampleProject"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "realiti2D/Realiti2D/src",
+        "realiti2D/Realiti2D/thirdparty/spdlog/include",
+        "realiti2D/Realiti2D/thirdparty/SDL2-2.0.12/include",
+        "realiti2D/Realiti2D/thirdparty/glew-2.1.0/include",
+        "realiti2D/Realiti2D/thirdparty/soil-0.0.3/include",
+        "realiti2D/Realiti2D/thirdparty/glm",
+        "realiti2D/Realiti2D/thirdparty/rapidjson-1.1.0/include"
+    }
+
+    links
+    {
+        "Realiti2D"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines 
+        {
+        }
+
+    filter "configurations:Debug"
+        defines "R2D_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "R2D_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "R2D_DIST"
+        optimize "On"
